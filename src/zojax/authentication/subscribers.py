bc93af74cc3f3653cd6ac7180264a2ae72cadf74
Dmitry Suvorov <suvdim@gmail.com>
2011-11-04 22:57:42 +0400

    #187: adding/deleting a cookie to login/logout

--- src/zojax/authentication/subscribers.py
+++ src/zojax/authentication/subscribers.py
@@ -15,13 +15,19 @@
 
 $Id$
 """
+import time
 from zope import component
 from zope.component import getUtility
+from zope.app.http.httpdate import build_http_date
+from zope.session.interfaces import IClientIdManager
 from zope.security.management import queryInteraction
 from zope.app.security.interfaces import IAuthentication
 
-from interfaces import IPrincipalInfoStorage, IPrincipalRemovingEvent
+from interfaces import IPrincipalInfoStorage, IPrincipalRemovingEvent, \
+                       IPrincipalLoggedInEvent, IPrincipalLoggingOutEvent
 
+cookie_name = '__ac_zojax'
+cookie_path = '/'
 
 @component.adapter(IPrincipalRemovingEvent)
 def principalRemovingHandler(event):
@@ -34,3 +40,26 @@ def principalRemovingHandler(event):
         if interaction is not None:
             for request in interaction.participations:
                 storage.clear(auth, request, principal)
+
+@component.adapter(IPrincipalLoggedInEvent)
+def cookieSet(event):
+    """"""
+    interaction = queryInteraction()
+    if interaction is not None and interaction.participations:
+        request = interaction.participations[0]
+        manager = component.getUtility(IClientIdManager)
+        if manager.cookieLifetime:
+            expires = build_http_date(time.time() + manager.cookieLifetime)
+        else:
+            expires = 'Tue, 19 Jan 2038 00:00:00 GMT'
+
+        request.response.setCookie(cookie_name, 'authorized', path=cookie_path, expires=expires)
+
+@component.adapter(IPrincipalLoggingOutEvent)
+def cookieDel(event):
+    """"""
+    interaction = queryInteraction()
+    if interaction is not None and interaction.participations:
+        request = interaction.participations[0]
+        request.response.expireCookie(cookie_name, path=cookie_path)
+
